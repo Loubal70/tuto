82b564755fb6893adad7f043b557ea577c4ce6e4
loubal70 <louisbal70@gmail.com>
2022-05-24 21:57:31 +0200

    add js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+window.onload = () => {
+    let hauteur = document.documentElement.scrollHeight - window.innerHeight;
+    let position = window.scrollY;
+    let largeur = document.documentElement.clientWidth;
+
+    let barre = (position /hauteur) * largeur;
+
+    document.getElementById("barre").style.width = barre + "px";
+}
