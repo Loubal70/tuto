f4e8dbc91c7865fd1654dfef8829be805e3987a2
loubal70 <louisbal70@gmail.com>
2022-05-28 09:45:01 +0200

    exo 3

--- resources/views/cours/3-activite3.blade.php
+++ resources/views/cours/3-activite3.blade.php
@@ -5,9 +5,6 @@
         <p class="text">   
             {{ __("exo3.In this tutorial, we will see how to animate our page with particles (falling snow).")}}
         </p>
-        <p class="text mt-10">
-            {{ __("exo2.For this exercise, you will have to develop your project on codepen!")}}
-        </p>
 
         <div class="aspect-w-16 aspect-h-9">
             <iframe src="https://www.youtube.com/embed/GN_92fWPJh4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width: 100%; height: 450px; margin-bottom: 3rem"></iframe>
