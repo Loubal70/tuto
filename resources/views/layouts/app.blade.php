cbcf7cf429c6391a2bcc9e0926e5d3c6fdda4b04
loubal70 <louisbal70@gmail.com>
2022-05-28 09:22:03 +0200

    amj server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,9 @@
                                 <a href="{{ route('activite2') }}" class="menu__item {{ (request()->routeIs('activite2')) ? 'active' : "" }}">{{ __('exo2.Activity 1') }}</a>
                                 @endif
                                 
-                                @if ($exercices[2]->allow == 1)
+                                {{ dd($exercices) }}
+
+                                {{-- @if ($exercices[2]->allow == 1) --}}
                                 <div class="menu__submenu">
                                     <a href="{{ route('getting_started') }}" class="menu__item {{ (request()->routeIs('getting_started')) ? 'active' : "" }}">{{ __('dashboard.Getting started') }}</a>
                                     <ul>
@@ -88,7 +90,7 @@
                                         </li>
                                     </ul>
                                 </div>
-                                @endif
+                                {{-- @endif --}}
 
             
                             </ul>
